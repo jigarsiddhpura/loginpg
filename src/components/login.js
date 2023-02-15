b750428e9f6feb7afc2b01ae379f19757e9ae6b8
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2023-02-15 19:05:21 +0530

    commit 2

--- src/components/login.js
+++ src/components/login.js
@@ -34,6 +34,7 @@ import {
 } from "@react-spring/web";
 import "./login.css";
 
+
 const theme = createTheme({
   palette: {
     naigara: {
@@ -98,171 +99,172 @@ const Login = () => {
     "&:hover": { backgroundColor: "#5E9387", color: "white" },
   });
 
-  // const imgStyle1 = {
-  //   backgroundImage:
-  //     "url('https://cdn.pixabay.com/photo/2020/11/26/15/28/woman-5779323_960_720.jpg')",
-  //   height: "100vh",
-  //   // width: "100%",
-  //   backgroundSize: "cover",
-  //   backgroundRepeat: "no-repeat",
-  //   backgroundPosition: "center",
-  //   opacity: "0.9",
-  //   // opacity: opacity.to(o => 1 - o), transform
-  // };
+  const [open, setOpen] = useState(true);
 
-  // const imgStyle2 = {
-  //   backgroundImage:
-  //     "url('https://cdn.pixabay.com/photo/2018/11/29/20/01/nature-3846403_960_720.jpg')",
-  //   height: "100vh",
-  //   // width: "100%",
-  //   backgroundSize: "cover",
-  //   backgroundRepeat: "no-repeat",
-  //   backgroundPosition: "center",
-  //   opacity: "0.9",
-  //   // opacity,transform,rotateX: '180deg',
-  // };
+  const [registrationStatus, setRegistrationStatus] = useState(true);
+  const [registrationFStatus, setFRegistrationStatus] = useState(true);
 
-  const [flipped, set] = useState(false);
-
-  const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 500, friction: 80 },
-  });
-
-  const Trail: React.FC<{ open: boolean }> = ({ open, children }) => {
-    const items = React.Children.toArray(children);
-    const trail = useTrail(items.length, {
-      config: { mass: 5, tension: 2000, friction: 200 },
-      opacity: open ? 1 : 0,
-      x: open ? 0 : 20,
-      height: open ? 110 : 0,
-      from: { opacity: 0, x: 20, height: 0 },
-    });
-    return (
-      <div>
-        {trail.map(({ height, ...style }, index) => (
-          <a.div key={index} className="trailsText">
-            <a.div style={{ height }}>{items[index]}</a.div>
-          </a.div>
-        ))}
-      </div>
-    );
+  const changeRegStat = () => {
+    setTimeout(() => {
+      setRegistrationStatus(!registrationStatus);
+    }, 1110);
+  };
+  const changeFRegStat = () => {
+    setFRegistrationStatus(!registrationFStatus);
+    changeRegStat();
   };
 
-  const [open, setOpen] = useState(true);
+  const flipDur = 1000;
+
+  const AnimatedGrid = animated(Grid);
+//   login img
+  const trails = useTrail(1, {
+    // rotateX: registrationFStatus ? 90 : 180,
+    opacity : registrationFStatus ? 0 : 1,
+    from: {opacity: 0 , transform: registrationFStatus ?'translateX(50vw)' : 'translateX(100vw)'},
+    to: { opacity:1, transform: registrationFStatus ? 'translateX(0vw)' :  "translateX(0vw)" },
+    // from: { opacity: 0 },
+    // enter: { opacity: 1 }
+    // leave: { opacity: 0 },
+    config: { duration: registrationFStatus ? flipDur + 1000: flipDur + 2000 , delay:800},
+    // config:{duration:flipDur}
+  });
+// signup img
+  const trails2 = useTrail(1, {
+    // rotateX: registrationFStatus ? 0 : 90,
+    opacity : registrationFStatus ? 1 : 0,
+    from: {opacity: 0 , transform: registrationFStatus ? '' : 'translateX(0vw)'},
+    to: { opacity:1, transform: registrationFStatus ? '' : 'translateX(50vw)'},
+    // from: { opacity: 0 },
+    // enter: { opacity: 1 },
+    // leave: { opacity: 0 },
+    config: { duration: registrationFStatus ? flipDur + 1000 : flipDur , delay:800},
+    // config:{duration:flipDur}
+  });
+  const formTrails = useTrail(1, {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // rotateX: registrationFStatus ? 90 : 180,
+    opacity : registrationFStatus ? 0 : 1,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: registrationFStatus ? flipDur : flipDur + 2000 },
+    // config:{duration:flipDur}
+  });
+  const formTrails2 = useTrail(1, {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // rotateX: registrationFStatus ? 0 : 90,
+    opacity : registrationFStatus ? 1 : 0,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: registrationFStatus ? flipDur + 2000 : flipDur },
+    // config:{duration:flipDur}
+  });
 
   const SignUpComponent = () => {
     return (
       <Grid container spacing={0}>
-        <Grid
-          item
-          xs={6}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            sx={{
-              maxWidth: 500,
-              width: 500,
-              height: 500,
-              //   border: "2px solid black",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              padding: "1rem",
-            }}
+                {trails2.map((props) => (
+          <AnimatedGrid
+            item
+            xs={6}
+            style={props}
+            className="img2 c"
+          ></AnimatedGrid>
+        ))}
+        {formTrails2.map((props) => (
+          <AnimatedGrid
+            item
+            xs={6}
+            style={props}
+            // className="formGrid"
           >
-            <p style={{ color: "gray" }}>START FOR FREE</p>
-            <h1 style={{ margin: "4px 0" }}>Create new account</h1>
-            <span style={{ color: "gray" }}>
-              Already A Member?{" "}
-              <a
-                href="#"
-                style={{ textDecoration: "none", color: color }}
-                onClick={() => {
-                  // set((state) => !state);
-                  // setOpen((state) => !state);
-                }}
-              >
-                Log in
-              </a>{" "}
-            </span>
-            <div style={{ display: "flex" }}>
-              <TextField
-                id=""
-                label="First Name"
-                style={{ margin: "1rem 0" }}
-              />
-              <TextField id="" label="Last Name" style={{ margin: "1rem " }} />
-            </div>
-            <FormControl sx={{ width: "50ch" }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-
-            <FormControl
-              style={{ margin: "1rem 0", width: "50ch" }}
-              variant="outlined"
+            <Box
+              sx={{
+                maxWidth: 500,
+                width: 500,
+                height: 500,
+                //   border: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                padding: "1rem",
+              }}
             >
-              <InputLabel htmlFor="outlined-adornment-password">
-                Email
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-email"
-                endAdornment={
-                  <InputAdornment position="end">
-                    <EmailOutlinedIcon />
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-            <div style={{ display: "flex" }}>
-              <ChangeButton>Change method</ChangeButton>
-              <SignupButton>Create account</SignupButton>
-            </div>
-          </Box>
-        </Grid>
-        <Grid item xs={6} className="img2 c"></Grid>
+              <p style={{ color: "gray" }}>START FOR FREE</p>
+              <h1 style={{ margin: "4px 0" }}>Create new account</h1>
+              <span style={{ color: "gray" }}>
+                Already A Member?{" "}
+                <a href="#" style={{ textDecoration: "none", color: color }}>
+                  Log in
+                </a>{" "}
+              </span>
+              <div style={{ display: "flex" }}>
+                <TextField
+                  id=""
+                  label="First Name"
+                  style={{ margin: "1rem 0" }}
+                />
+                <TextField
+                  id=""
+                  label="Last Name"
+                  style={{ margin: "1rem " }}
+                />
+              </div>
+              <FormControl sx={{ width: "50ch" }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
 
-        {/* Below code is for IMG transition but not working  WHEN COMBINED with other */}
-
-        {/* <Grid item xs={6} className="img2 c">
-          <a.div
-            className="img1"
-            style={{ opacity: opacity.to((o) => 1 - o), transform }}
-          />
-          <a.div
-            className="img2"
-            style={{
-              opacity,
-              transform,
-              rotateX: "180deg",
-            }}
-          />
-        </Grid> */}
+              <FormControl
+                style={{ margin: "1rem 0", width: "50ch" }}
+                variant="outlined"
+              >
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Email
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-email"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <EmailOutlinedIcon />
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <div style={{ display: "flex" }}>
+                <ChangeButton onClick={changeFRegStat}>
+                  Change method
+                </ChangeButton>
+                <SignupButton>Create account</SignupButton>
+              </div>
+            </Box>
+          </AnimatedGrid>
+        ))}
       </Grid>
     );
   };
@@ -270,111 +272,95 @@ const Login = () => {
   const LoginComponent = () => {
     return (
       <Grid container spacing={0}>
-        <Grid
-          item
-          xs={6}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            sx={{
-              maxWidth: 500,
-              width: 500,
-              height: 500,
-              //   border: "2px solid black",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              padding: "1rem",
-            }}
-          >
-            <p style={{ color: "gray" }}>START FOR FREE</p>
-            <h1 style={{ margin: "4px 0" }}>Welcome back!</h1>
-
-            <FormControl
-              style={{ margin: "1rem 0", width: "50ch" }}
-              variant="outlined"
+        {formTrails.map((props) => (
+          <AnimatedGrid item xs={6} style={props}>
+            <Box
+              sx={{
+                maxWidth: 500,
+                width: 500,
+                height: 500,
+                //   border: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                padding: "1rem",
+              }}
             >
-              <InputLabel htmlFor="outlined-adornment-password">
-                Email
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-email"
-                endAdornment={
-                  <InputAdornment position="end">
-                    <EmailOutlinedIcon />
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-            <FormControl sx={{ width: "50ch" }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
+              <p style={{ color: "gray" }}>START FOR FREE</p>
+              <h1 style={{ margin: "4px 0" }}>Welcome back!</h1>
 
-            <span style={{ color: "gray", margin: "1rem 0" }}>
-              New here?
-              <a
-                href=""
-                style={{ textDecoration: "none", color: color }}
-                onClick={() => {
-                  // state change for both transitions below on click but not working
-                  // set((state) => !state);
-                  // setOpen((state) => !state);
-                }}
+              <FormControl
+                style={{ margin: "1rem 0", width: "50ch" }}
+                variant="outlined"
               >
-                Sign Up
-              </a>
-            </span>
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Email
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-email"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <EmailOutlinedIcon />
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <FormControl sx={{ width: "50ch" }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
 
-            <div style={{ display: "flex" }}>
-              <ChangeButton>Change method</ChangeButton>
-              <SignupButton>Log in</SignupButton>
-            </div>
-          </Box>
-        </Grid>
-        <Grid item xs={6} className="img1 c"></Grid>
+              <span style={{ color: "gray", margin: "1rem 0" }}>
+                New here?
+                <a href="" style={{ textDecoration: "none", color: color }}>
+                  Sign Up
+                </a>
+              </span>
+
+              <div style={{ display: "flex" }}>
+                <ChangeButton onClick={changeFRegStat}>
+                  Change method
+                </ChangeButton>
+                <SignupButton>Log in</SignupButton>
+              </div>
+            </Box>
+          </AnimatedGrid>
+        ))}
+        {trails.map((props) => (
+          <AnimatedGrid
+            item
+            xs={6}
+            style={props}
+            className="img1 c"
+          ></AnimatedGrid>
+        ))}
       </Grid>
     );
   };
 
   return (
     <>
-      <SignUpComponent />
-      <LoginComponent />
-
-      {/* Below code is for FORM transition but not working  */}
-
-      {/* <div>
-        <Trail open={open}>
-          <SignUpComponent />
-        </Trail>
-        <Trail open={!open}>
-          <LoginComponent />
-        </Trail>
-      </div> */}
+      {registrationStatus && <SignUpComponent />}
+      {!registrationStatus && <LoginComponent />}
     </>
   );
 };
